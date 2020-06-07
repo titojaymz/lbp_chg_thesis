d9f763d42762cd75622c15aec26fd4bf617ca537
Josef <sepb.99@gmail.com>
2020-06-07 11:38:32 +0800

    count of FBs

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -119,4 +119,52 @@ class dashboard extends CI_Controller
         $this->load->view('dashboard_list2',$data);
         $this->load->view('footer');
     }
+
+    public function index2($operation = NULL)
+    {
+        if (!$this->session->userdata('user_data')){
+            redirect('user','location');
+        }
+
+        if($operation == NULL)
+        {
+            $message = '';
+        }
+        else
+        {
+            if($operation == 'landaddsuccess')
+            {
+                $message = 'Add success';
+            }
+            else if($operation == 'norecord')
+            {
+                $message = 'No record found';
+            }
+            else if($operation == 'landupdatesuccess')
+            {
+                $message = 'Update success';
+            }
+            else
+            {
+                $message = '';
+            }
+        }
+
+        $landRegList = new Land_class_model();
+        $reports = new Reports_model();
+
+        $data = array(
+            'system_message' => $message,
+            'access_level' => $this->accessLevel(),
+            'page_title' => $this->title(),
+            'landclass' => $reports->getLandClass(),
+            'apprv_claims' => $reports->report2()
+        );
+
+        $this->load->view('header');
+        $this->load->view('navbar',$data);
+        $this->load->view('sidebar');
+        $this->load->view('dashboard_list3',$data);
+        $this->load->view('footer');
+    }
 }
